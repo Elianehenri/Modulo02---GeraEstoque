1c6c7558356b770a5fc2fe3c8f00488873dab186
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-06-24 16:13:45 -0300

    Criacao do formulario Exercicio 01

---
+++ Semana04/ExercicioSemana04Mod02/ExercicioSemana04Mod02/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ExercicioSemana04Mod02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEscolaridade = new System.Windows.Forms.TextBox();
+            this.txtDataDeNascimento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtValorCurso = new System.Windows.Forms.TextBox();
+            this.txtValorDesconto = new System.Windows.Forms.TextBox();
+            this.txtValorMulta = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSalvarDados = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtNome.Location = new System.Drawing.Point(12, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(295, 23);
+            this.txtNome.TabIndex = 0;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Curso";
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtCurso.Location = new System.Drawing.Point(12, 214);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(295, 23);
+            this.txtCurso.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Escolaridade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Data de Nascimento";
+            // 
+            // txtEscolaridade
+            // 
+            this.txtEscolaridade.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtEscolaridade.Location = new System.Drawing.Point(12, 150);
+            this.txtEscolaridade.Name = "txtEscolaridade";
+            this.txtEscolaridade.Size = new System.Drawing.Size(295, 23);
+            this.txtEscolaridade.TabIndex = 6;
+            // 
+            // txtDataDeNascimento
+            // 
+            this.txtDataDeNascimento.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtDataDeNascimento.Location = new System.Drawing.Point(12, 89);
+            this.txtDataDeNascimento.Name = "txtDataDeNascimento";
+            this.txtDataDeNascimento.Size = new System.Drawing.Size(200, 23);
+            this.txtDataDeNascimento.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valor Curso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Valor Desconto";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Valor Multa";
+            // 
+            // txtValorCurso
+            // 
+            this.txtValorCurso.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtValorCurso.Location = new System.Drawing.Point(12, 282);
+            this.txtValorCurso.Name = "txtValorCurso";
+            this.txtValorCurso.Size = new System.Drawing.Size(81, 23);
+            this.txtValorCurso.TabIndex = 11;
+            // 
+            // txtValorDesconto
+            // 
+            this.txtValorDesconto.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtValorDesconto.Location = new System.Drawing.Point(121, 282);
+            this.txtValorDesconto.Name = "txtValorDesconto";
+            this.txtValorDesconto.Size = new System.Drawing.Size(81, 23);
+            this.txtValorDesconto.TabIndex = 12;
+            // 
+            // txtValorMulta
+            // 
+            this.txtValorMulta.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtValorMulta.Location = new System.Drawing.Point(226, 282);
+            this.txtValorMulta.Name = "txtValorMulta";
+            this.txtValorMulta.Size = new System.Drawing.Size(81, 23);
+            this.txtValorMulta.TabIndex = 13;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtIdade.Enabled = false;
+            this.txtIdade.Location = new System.Drawing.Point(226, 89);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(81, 23);
+            this.txtIdade.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(247, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Idade";
+            // 
+            // btnSalvarDados
+            // 
+            this.btnSalvarDados.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSalvarDados.Location = new System.Drawing.Point(12, 320);
+            this.btnSalvarDados.Name = "btnSalvarDados";
+            this.btnSalvarDados.Size = new System.Drawing.Size(295, 26);
+            this.btnSalvarDados.TabIndex = 16;
+            this.btnSalvarDados.Text = "Salvar Dados";
+            this.btnSalvarDados.UseVisualStyleBackColor = false;
+            this.btnSalvarDados.Click += new System.EventHandler(this.btnSalvarDados_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 362);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Lista de Cursos";
+            // 
+            // txtLista
+            // 
+            this.txtLista.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtLista.Location = new System.Drawing.Point(12, 380);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(313, 64);
+            this.txtLista.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLista);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnSalvarDados);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtValorMulta);
+            this.Controls.Add(this.txtValorDesconto);
+            this.Controls.Add(this.txtValorCurso);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDataDeNascimento);
+            this.Controls.Add(this.txtEscolaridade);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCurso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private Label label1;
+        private Label label2;
+        private TextBox txtCurso;
+        private Label label3;
+        private Label label4;
+        private TextBox txtEscolaridade;
+        private TextBox txtDataDeNascimento;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtValorCurso;
+        private TextBox txtValorDesconto;
+        private TextBox txtValorMulta;
+        private TextBox txtIdade;
+        private Label label8;
+        private Button btnSalvarDados;
+        private Label label9;
+        private TextBox txtLista;
+    }
+}
---
+++ Semana04/ExercicioSemana04Mod02/ExercicioSemana04Mod02/Form1.cs
@@ -0,0 +1,18 @@
+namespace ExercicioSemana04Mod02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        //momento de simular a inscriçao do  curso
+        private void btnSalvarDados_Click(object sender, EventArgs e)
+        {
+            //codigo
+            MessageBox.Show("Eliane!!!!");
+        }
+    }
+}
