00288b7e036cb75d8791f511d2018a0be150057a
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-06-25 18:23:14 -0300

    Criar uma classe FichaInscricao publica

---
+++ Semana04/ExercicioSemana04Mod02/ExercicioSemana04Mod02/FichaDeInscricao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioSemana04Mod02
+{
+    class FichaDeInscricao
+    {
+        #region propriedades
+        public string Nome { get; set; }
+        public int Idade { get; }
+        public string Curso { get; set; }
+        public string Escolaridade { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public decimal ValorCurso { get; set; }
+        public decimal ValorDesconto { get; set; }
+        public decimal ValorMulta { get; set; }
+
+        #endregion
+    }
+}
