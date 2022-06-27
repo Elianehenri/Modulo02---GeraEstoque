f5e7c5e64e81c4b55a3115085b9a7e7d7adb5886
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-06-27 13:36:58 -0300

    Criar uma classe FichaInscricao publica

---
+++ Semana04/FichaDeInscricao/FichaDeInscricao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FichaDeInscricao
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ficha de Inscriçao");
+            Console.ReadLine();
+
+        }
+    }
+}
