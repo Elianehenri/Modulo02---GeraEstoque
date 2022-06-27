0e11de9fa9446eeae0a9f10561b87091d54b1fa8
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-06-27 14:42:08 -0300

    Criar um método privado Calculo na classe CobrarCurso

--- Semana04/FichaDeInscricao/FichaDeInscricao/CobrarCurso.cs
+++ Semana04/FichaDeInscricao/FichaDeInscricao/CobrarCurso.cs
@@ -9,19 +9,19 @@ namespace FichaDeInscricao
         private double valorDesconto;
         private double resultado;
         ////exercicio 07
-        //public CobrarCurso(double valorCurso, double valorMulta, double valorDesconto)
-        //{
-        //    this.valorCurso = valorCurso;
-        //    this.valorMulta = valorMulta;
-        //    this.valorDesconto = valorDesconto;
-        //}
+        public CobrarCurso(double valorCurso, double valorMulta, double valorDesconto)
+        {
+            this.valorCurso = valorCurso;
+            this.valorMulta = valorMulta;
+            this.valorDesconto = valorDesconto;
+        }
 
         //public double ValorCurso { get; set; }
         //public double ValorMulta { get; set; }
         //public double ValorDesconto { get; set; }
         //public double Resultado { get => this.resultado; }
 
-        ////exercicio 08
+        //exercicio 08
         //public void CalcularResultado()
         //{
         //    resultado = valorMulta > 0 ? valorCurso + valorMulta : valorCurso - valorDesconto;
