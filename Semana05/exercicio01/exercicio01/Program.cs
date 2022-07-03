3607cdb1403e4e98b6256959eef8fe33adf5a301
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-07-03 15:03:31 -0300

    Exercicios da Semana 05

---
+++ Semana05/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace e // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicios Semana 05 Professor Jaime - Herança!");
+            Console.WriteLine( "***********************************************");
+        }
+    }
+}
