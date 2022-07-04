0aa66ae9225d51149beda981e7b22c4b1f2a838b
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-07-03 22:21:14 -0300

    Exercicios 02 a 04 da Semana 05

--- Semana05/Cartoes/Cartoes/Program.cs
+++ Semana05/Cartoes/Cartoes/Program.cs
@@ -9,7 +9,7 @@ namespace Cartoes
             Console.WriteLine("Exercicios 02 a 04 da Semana 05!");
             Console.WriteLine("********************************");
 
-            //
+            
             CartaoVR cartaoVR = new CartaoVR("MasterCard", 2000, 0.0);
             Console.WriteLine($" O cartao VR da Bandeira: { cartaoVR.Bandeira}, tem saldo disponivel de limite { cartaoVR.Saldo.ToString("F2", CultureInfo.InvariantCulture)}  , com uma taxa de {cartaoVR.VerificarValorTaxaCartao().ToString("c")}.");
             Console.WriteLine();     
