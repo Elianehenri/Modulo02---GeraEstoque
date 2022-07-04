0619106026388eea0faff0eef1d02085f90476c9
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-07-04 14:14:19 -0300

    Exercicios 05 e 06 da semana 05

---
+++ Semana05/TratamentoExcecoes/TratamentoExcecoes/Entities/Funcionario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratamentoExcecoes.Entities
+{
+    class Funcionario
+    {
+        //propriedade
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        //construtor
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(string id, string name)
+        {
+            Id = id;
+            Nome = name;
+        }
+
+
+        
+
+    }
+    [Serializable]
+    public class NomeFuncionarioInvalidoException : Exception
+    {
+        public NomeFuncionarioInvalidoException()
+        {
+
+        }
+        public NomeFuncionarioInvalidoException(string nome) : base(string.Format($"Nome Funcionario Invalido: {nome}"))
+        {
+
+        }
+    }
+
+    public class IdFuncionarioInvalidoException : Exception
+    {
+        public IdFuncionarioInvalidoException()
+        {
+
+        }
+
+        public IdFuncionarioInvalidoException(string id) : base(string.Format($"ID do funcionario é invalido: {id}"))
+        {
+
+        }
+    }
+}
---
+++ Semana05/TratamentoExcecoes/TratamentoExcecoes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using TratamentoExcecoes.Entities;
+
+namespace TratamentoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Exercicios 05 e 06 da Semana 05!");
+            Console.WriteLine("*********************************");
+
+            Funcionario funcionario = new Funcionario("12345", "Eliane");
+            Funcionario funcionario1 = new Funcionario(" ", " ");
+
+
+            Console.Write("Digite o nome do funcionario:");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Digite o ID do funcionario:");
+            funcionario1.Id = Console.ReadLine();
+
+            try
+            {
+                ValidarFuncionario(funcionario);
+                ValidarNomeFuncionario(funcionario1);
+                ValidarIdFuncionario(funcionario1);
+
+
+                Console.WriteLine($"ID: {funcionario.Id}, Nome: {funcionario.Nome} ");
+                Console.WriteLine($"ID: {funcionario1.Id}, Nome: {funcionario1.Nome} ");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ValidarFuncionario(Funcionario funcionario)
+        {
+            if (funcionario == null)
+            {
+                throw new NullReferenceException(" O Funcionario é Null!");
+            }
+        }
+
+        private static void ValidarNomeFuncionario(Funcionario funcionario1)
+        {
+            Regex regexNome = new Regex("^[async-zA-Z]+$");
+            if (!regexNome.IsMatch(funcionario1.Nome))
+
+            {
+                throw new NomeFuncionarioInvalidoException(funcionario1.Nome);
+            }
+        }
+        private static void ValidarIdFuncionario(Funcionario funcionario1)
+        {
+            Regex regexId = new Regex("^[0-9]+$");
+            if (!regexId.IsMatch(funcionario1.Id))
+            {
+                throw new IdFuncionarioInvalidoException(funcionario1.Id);
+            }
+        }
+    }
+}
