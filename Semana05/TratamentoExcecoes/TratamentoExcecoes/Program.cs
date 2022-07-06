1fe7d10ce57954471dfa33e2fb83d902a787597e
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-07-06 15:39:33 -0300

    Estrutura do Projeto

--- Semana05/TratamentoExcecoes/TratamentoExcecoes/Program.cs
+++ Semana05/TratamentoExcecoes/TratamentoExcecoes/Program.cs
@@ -4,64 +4,64 @@ using TratamentoExcecoes.Entities;
 
 namespace TratamentoExcecoes
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Exercicios 05 e 06 da Semana 05!");
-            Console.WriteLine("*********************************");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine();
+			Console.WriteLine("Exercicios 05 e 06 da Semana 05!");
+			Console.WriteLine("*********************************");
 
-            Funcionario funcionario = new Funcionario("12345", "Eliane");
-            Funcionario funcionario1 = new Funcionario(" ", " ");
-
-
-            Console.Write("Digite o nome do funcionario:");
-            funcionario1.Nome = Console.ReadLine();
-            Console.Write("Digite o ID do funcionario:");
-            funcionario1.Id = Console.ReadLine();
-
-            try
-            {
-                ValidarFuncionario(funcionario);
-                ValidarNomeFuncionario(funcionario1);
-                ValidarIdFuncionario(funcionario1);
+			Funcionario funcionario = new Funcionario("12345", "Eliane");
+			Funcionario funcionario1 = new Funcionario(" ", " ");
 
 
-                Console.WriteLine($"ID: {funcionario.Id}, Nome: {funcionario.Nome} ");
-                Console.WriteLine($"ID: {funcionario1.Id}, Nome: {funcionario1.Nome} ");
-            }
-            catch (Exception ex)
-            {
+			Console.Write("Digite o nome do funcionario:");
+			funcionario1.Nome = Console.ReadLine();
+			Console.Write("Digite o ID do funcionario:");
+			funcionario1.Id = Console.ReadLine();
 
-                Console.WriteLine(ex.Message);
-            }
-        }
+			try
+			{
+				ValidarFuncionario(funcionario);
+				ValidarNomeFuncionario(funcionario1);
+				ValidarIdFuncionario(funcionario1);
 
-        private static void ValidarFuncionario(Funcionario funcionario)
-        {
-            if (funcionario == null)
-            {
-                throw new NullReferenceException(" O Funcionario é Null!");
-            }
-        }
 
-        private static void ValidarNomeFuncionario(Funcionario funcionario1)
-        {
-            Regex regexNome = new Regex("^[async-zA-Z]+$");
-            if (!regexNome.IsMatch(funcionario1.Nome))
+				Console.WriteLine($"ID: {funcionario.Id}, Nome: {funcionario.Nome} ");
+				Console.WriteLine($"ID: {funcionario1.Id}, Nome: {funcionario1.Nome} ");
+			}
+			catch (Exception ex)
+			{
 
-            {
-                throw new NomeFuncionarioInvalidoException(funcionario1.Nome);
-            }
-        }
-        private static void ValidarIdFuncionario(Funcionario funcionario1)
-        {
-            Regex regexId = new Regex("^[0-9]+$");
-            if (!regexId.IsMatch(funcionario1.Id))
-            {
-                throw new IdFuncionarioInvalidoException(funcionario1.Id);
-            }
-        }
-    }
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private static void ValidarFuncionario(Funcionario funcionario)
+		{
+			if (funcionario == null)
+			{
+				throw new NullReferenceException(" O Funcionario é Null!");
+			}
+		}
+
+		private static void ValidarNomeFuncionario(Funcionario funcionario1)
+		{
+			Regex regexNome = new Regex("^[async-zA-Z]+$");
+			if (!regexNome.IsMatch(funcionario1.Nome))
+
+			{
+				throw new NomeFuncionarioInvalidoException(funcionario1.Nome);
+			}
+		}
+		private static void ValidarIdFuncionario(Funcionario funcionario1)
+		{
+			Regex regexId = new Regex("^[0-9]+$");
+			if (!regexId.IsMatch(funcionario1.Id))
+			{
+				throw new IdFuncionarioInvalidoException(funcionario1.Id);
+			}
+		}
+	}
 }
