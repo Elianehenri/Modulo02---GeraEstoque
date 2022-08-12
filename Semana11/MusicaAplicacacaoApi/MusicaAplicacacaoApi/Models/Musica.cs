669e334a29769b5ff6c14a559f62ed57a391141f
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-08-12 15:02:22 -0300

    Exercicio 03

---
+++ Semana11/MusicaAplicacacaoApi/MusicaAplicacacaoApi/Models/Musica.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicaAplicacacaoApi.Models
+{
+    public class Musica
+    {
+        public int Id { get; internal set; }
+        [Required(ErrorMessage = "O nome do artista é obrigatório.")]
+        public string Nome { get; set; }
+        public double Duracao { get; set; }
+        //public TimeSpan Duracao { get; set; }
+        public Album Album { get; set; }
+        public Artista Artista { get; set; }
+
+        public Musica()
+        {
+        }
+
+        public Musica(string nome, double duracao, Artista artista)
+        {
+            Nome = nome;
+            Duracao = duracao;
+            Artista = artista;
+        }
+
+    }
+}
